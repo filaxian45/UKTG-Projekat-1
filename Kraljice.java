5084817dd043f7b93dd8d3d51818d0879295f6ae
Filip <filipturajlic@gmail.com>
2026-06-14 22:09:03 +0200

    zavrsena funkcionalnost, program daje ocekivan 12 resenja

--- Kraljice.java
+++ Kraljice.java
@@ -1,7 +1,7 @@
 public class Kraljice{
     /*
     dalji koraci,
-    svih 8 transformacija (laki deo)
+    4/7 transformacija (laki deo)
     poredjenje
     dodati da ubacuje u rez ako nijedan od tih nije vec u njemu
     */
@@ -29,7 +29,7 @@ public class Kraljice{
     private static int[][] transform1D2D(int[] w, int n){//pretvara 1d niz u 2d niz
         int[][] nov = new int[n][n];
         for(int i = 0; i< n;i++){
-            nov[i][w[i]] = w[i]+1;
+            nov[i][w[i]] = w[i]+1; //dodato da bi 0 se mogla naci u 2d -> 1d
         }
         return nov;
     }
@@ -38,7 +38,7 @@ public class Kraljice{
         for(int i = 0; i < n; i++){
             for(int j = 0; j <n; j++){
                 if(w[i][j] != 0){
-                    nov[i] = w[i][j] - 1;
+                    nov[i] = j; //prvobitno je bilo w[i][j] -1, sto je dovodilo 
                 }
             }
         } 
@@ -90,7 +90,6 @@ public class Kraljice{
         int n = w.length;
         int temp;
         int[][] nov = transform1D2D(w, n);
-        ispisiMatricu(nov, n);
         for(int i = 0; i <n; i++){ //transponovanje matrice
             for(int j = i; j<n;j++){
                 temp = nov[j][i];
@@ -105,8 +104,62 @@ public class Kraljice{
                 nov[n-j-1][i] = temp;
             }
         }
-        System.out.println("LINEBREAKER");
-        ispisiMatricu(nov, n);
+        int[] output = transform2D1D(nov, n);
+        return output;
+    }
+    private static int[] flipV(int[] w){
+        int n = w.length;
+        int temp;
+        int[][] nov = transform1D2D(w, n);
+        for(int i = 0;i<n;i++){//obrcu se kolone
+            for(int j =0; j<n/2;j++){
+                temp = nov[j][i];
+                nov[j][i] = nov[n-j-1][i];
+                nov[n-j-1][i] = temp;
+            }
+        }
+        int[] output = transform2D1D(nov, n);
+        return output;
+    }
+    private static int[] flipH(int[] w){
+        int n = w.length;
+        int temp;
+        int[][] nov = transform1D2D(w, n);
+        for(int k = 0; k < n;k++){ //obrcu se redovi
+            for(int l = 0; l < n/2;l++){
+                temp = nov[k][l];
+                nov[k][l] = nov[k][n-1-l];
+                nov[k][n-1-l] = temp;
+            }
+        }
+        int[] output = transform2D1D(nov, n);
+        return output;
+    }
+    private static int[] flipMainD(int[] w){
+         int n = w.length;
+        int temp;
+        int[][] nov = transform1D2D(w, n);
+        for(int i = 0; i <n; i++){ //transponovanje matrice, time se obrce ono sto je iznad Gl. diagonale i ono sto je ispod
+            for(int j = i; j<n;j++){
+                temp = nov[j][i];
+                nov[j][i] = nov[i][j];
+                nov[i][j] = temp;
+            }
+        }
+        int[] output = transform2D1D(nov, n);
+        return output;
+    }
+    private static int[] flipSecD(int[] w){
+         int n = w.length;
+        int temp;
+        int[][] nov = transform1D2D(w, n);
+        for(int i = 0; i <n; i++){ //transponovanje matrice, time se obrce ono sto je iznad Spor. diagonale i ono sto je ispod
+            for(int j = 0; j<n-1-i;j++){
+                temp = nov[i][j];
+                nov[i][j] = nov[n-1-j][n-1-i]; //stavljeno ovako zato sto ce n-1-i/j dati obrnuto od i/j trenutno u odnosu na Spor. dia, ide [i][j] sa [n-1-j][n-1-i] isto kao ij sa ji
+                nov[n-1-j][n-1-i] = temp;
+            }
+        }
         int[] output = transform2D1D(nov, n);
         return output;
     }
@@ -146,13 +199,33 @@ public class Kraljice{
     private static boolean obradaIs(int[][] res, int[] a){ //WIP, nema funkcionalnost za proveru izomorfnost
         for(int i = 0; i < res.length ; i++){
             if(res[i] != null){
-            if(isEqualNiz(res[i],rot90(a)) || isEqualNiz(res[i],rot180(a)) || isEqualNiz(res[i],rot270(a)) || /*TBD*/){
+                if(isEqualNiz(a,rot90(res[i]))){  
+                    return false;
+                }
+                if(isEqualNiz(a,rot180(res[i]))){ 
+                    return false;
+                }
+                if(isEqualNiz(a,rot270(res[i]))){   
+                    return false;
+                }
+                if(isEqualNiz(a,flipH(res[i]))){
+                    return false;
+                }
+                if(isEqualNiz(a,flipV(res[i]))){
+                    return false;
+                }
+                if(isEqualNiz(a, flipMainD(res[i]))){
+                    return false;
+                }
+                if(isEqualNiz(a, flipSecD(res[i]))){ 
+                    return false;
+                }
+                if(isEqualNiz(a, res[i])){  
                     return false;
                 }
             }
-       // }
+        }
         return true;
-
     }
     static void sledeci(int[] w, int n){
         int i = n-2;
@@ -175,8 +248,11 @@ public class Kraljice{
     static void obradi(int[] w, int n, int[][] res){
         if(NemaDijaNapada(w, n)){
             if(obradaIs(res,w)){
-                res[BR] = w;
+                res[BR] = w.clone();
                 BR++;
+                System.out.print(BR + ": ");
+                ispisiNiz(w);
+                
             }
         }
     }
@@ -184,22 +260,11 @@ public class Kraljice{
         int n = 8; //chessboard velicina
         int[][] rez = new int[100][];
         int[] w = new int[n];
-       // postaviPrvi(w,n);
-       // obradi(w,n);
-       // while(!poslednji(w,n)){
-       //    sledeci(w,n);
-       //    obradi(w,n); 
-      // }
-      w[0] = 0;
-      w[1] = 4;
-      w[2] = 7;
-      w[3] = 6;
-      w[4] = 3;
-      w[5] = 1;
-      w[6] = 2;
-      w[7] = 5;
-      ispisiNiz(w);
-      int[] r = rot270(w);
-      ispisiNiz(r);
+        postaviPrvi(w,n);
+        obradi(w,n,rez);
+        while(!poslednji(w,n)){
+            sledeci(w,n);
+            obradi(w,n,rez); 
+      }
     }
 }
